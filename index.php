c31bd2f2e5dbdb90b13c355217888e6b27ce16ea
Mikail <Mikailkalkanpro@gmail.com>
2021-01-31 17:46:18 +0100

    new CV

---
+++ index.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mikail Kalkan CV</title>
+    <meta name="description" content="Curiculum Vitae Mikail Kalkan" />
+    <meta name="author" content="Mikail" />
+
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
+  <body>
+    <header class="row justify-content-center m-1">
+      <div class="col">
+        <h1 class="text-left">Mikail Kalkan</h1>
+        <div class="row justify-content-evenly">
+          <h2 class="cv-title align-self-center">Developeur Web</h2>
+          <div class="boxedImg">
+            <img src="assets/images/miko cv.jpg" alt="image Mikail" />
+          </div>
+          <p class="align-self-center">
+            14 avenue du Dr Paul Pompidor<br />
+            11100 Narbonne<br />
+            <a href="tel:+0646673469">06.46.67.34.69</a><br />
+            <a href="mailto:mikailkalkanpro@gmail.com">
+              mikailkalkanpro@gmail.com
+            </a>
+          </p>
+        </div>
+        <div class="about">
+          <h2>À propos de moi</h2>
+          <hr />
+          <p>
+            De nature curieux, j’aime apprendre et découvrir les nouveautés.
+            Diplômé en STI2D au lycée Louise Michel, passionné des technologies,
+            sportifs et bricoleur, je suis à la recherche d’un stage de 3 mois
+            dans le Développement Web. À l’aise à l’oral, avec la clientèle je
+            peux converser dans plusieurs langues. Ayant pour objectif d’évoluer
+            dans l’entreprise, j’adore les défis. Travailleur, toujours avec le
+            sourire, j’apprécie le travail en équipe.
+          </p>
+        </div>
+      </div>
+    </header>
+
+    <!-- First Row -->
+    <div class="article-container row justify-content-between m-1">
+      <!-- Article Experience Professionel -->
+      <article class="middle-cut">
+        <h2>Éxperiences Professionnelles</h2>
+        <hr />
+        <div class="section-container">
+          <section>
+            <h3>Avril-Octobre|2019 KFC Narbonne - Équipier Polyvalent</h3>
+            <p>
+              Relation avec la clientèle (accueil, renseignements, aide à la
+              prise des commandes sur la borne et Comptoir)<br /><br />
+              Enregistrement, préparation des commandes (confection des
+              sandwichs, cuisson des aliments, dressage etc…)<br /><br />
+              Encaisser les ventes, responsable de caisse (vérification des
+              montants des fonds) <br /><br />
+              Entretien des locaux (nettoyage de la salle et équipements)
+              <br />
+            </p>
+          </section>
+          <section>
+            <h3>Avril-Novembre|2014 Grand Narbonne - Chantier Jeunes</h3>
+            <p>
+              Des chantiers destinés à améliorer l’environnement, à aménager ou
+              rénover l’espace public<br />
+              Réalisation de travaux d’aménagement<br />
+              Rénovation ponçage et peinture de mobiliers urbains. <br />
+            </p>
+          </section>
+          <section>
+            <h3>Fevrier|2012 C.N.N Narbonne - Maitre-Nageur</h3>
+            <p>
+              1 Semaine Stage Formation de Maitre-nageur.<br />
+              Veiller à la sécurité des personnes, enseigner l'apprentissage de
+              la nage.
+            </p>
+          </section>
+        </div>
+      </article>
+
+      <!-- Article Formation -->
+      <article class="middle-cut">
+        <h2>Formation</h2>
+        <hr />
+        <div class="section-container">
+          <section>
+            <h3>Octobre|2020 Pole INESS - Formation SIMPLON</h3>
+            <p>Développeur Web et Web mobile : En cours d’apprentissage.</p>
+          </section>
+          <section>
+            <h3>Octobre|2017 Mission Local - Formation S.S.T et H0/B0</h3>
+            <p>
+              S.S.T: Porter secours à une personne en détresse et participer
+              activement à la démarche de prévention au sein de l’entreprise.
+              <br />H0B0: Identifier, adopter la bonne conduite pour maîtriser
+              les risques électriques liés au travail.
+            </p>
+          </section>
+          <section>
+            <h3>2015-2017|Faculté des Science Montpellier 2</h3>
+            <p>Licence Mathématique niveaux 2 ans Montpellier.</p>
+          </section>
+          <section>
+            <h3>2014-2015|Lycée Diderot Narbonne - BAC S.T.I.2D</h3>
+            <p>Licence Mathématique niveaux 2 ans Montpellier.</p>
+          </section>
+          <section>
+            <h3>2012-2013|Lycée Diderot Narbonne - B.I.A</h3>
+            <p>
+              Brevet d’Initiation Aéronautique.<br />
+              Bonne culture dans le domaine aéronautique et spatial Expérience
+              pratique (Vol)..
+            </p>
+          </section>
+        </div>
+      </article>
+    </div>
+
+    <!-- Second Row -->
+    <div class="article-container row justify-content-between m-1">
+      <div class="col middle-custom justify-content-between">
+        <!-- Article Experience Professionel -->
+        <article>
+          <h2>Langues</h2>
+          <hr />
+          <div class="section-container">
+            <section>
+              <ul>
+                <li>
+                  <p>🇺🇸Anglais:</p>
+
+                  <progress class="prog" value="80" max="100"></progress>
+                </li>
+
+                <li>
+                  <p>🇪🇸 Espagnol:</p>
+
+                  <progress class="prog" value="40" max="100"></progress>
+                </li>
+
+                <li>
+                  <p>🇹🇷Turc:</p>
+
+                  <progress class="prog" value="55" max="100"></progress>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </article>
+
+        <!-- Article Hobbies -->
+        <article>
+          <h3>Hobbies</h3>
+          <hr />
+          <div class="section-container">
+            <section>
+              <p>
+                -Sportif: 12 ans de Natation, 8 ans de judo, 2 ans Boxe
+                Thaïe.<br />
+
+                -Technophile: Intérêt pour les nouvelles technologies.<br />
+
+                -Passionné et Curieux de science, cinéma, lecture et voyage.
+              </p>
+            </section>
+          </div>
+        </article>
+      </div>
+
+      <!-- Article Competence -->
+      <article class="middle-custom-2">
+        <h2>Compétence Perso</h2>
+        <hr />
+        <div class="section-container">
+          <div class="d-flex">
+            <ol>
+              <li>
+                <p>
+                  Creatif:
+                  <span class="rond">
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr2"> </span>
+                  </span class="rond">
+                </p>
+              </li>
+
+              <li>
+                <p>
+                  Curieux:
+                  <span class="rond">
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr2"> </span>
+                    <span class="sr2"> </span>
+                  </span class="rond">
+                </p>
+              </li>
+
+              <li>
+                <p>
+                  Sens de l'écoute:
+                  <span class="rond">
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                  </span>
+                </p>
+              </li>
+
+              <li>
+                <p>
+                  Sérieux:
+                  <span class="rond">
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr1"> </span>
+                    <span class="sr2"> </span>
+                  </span>
+                </p>
+              </li>
+            </ol>
+          </div>
+        </div>
+        <h2>Compétence Perso</h2>
+        <hr />
+        <div class="section-container">
+          <div class="d-flex">
+            <ol>
+              <li>
+                <p>
+                  Web Developement:
+                  <span class="rond">
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr4"> </span>
+                  </span>
+                </p>
+              </li>
+
+              <li>
+                <p>
+                  Photoshop:
+                  <span class="rond">
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr4"> </span>
+                    <span class="sr4"> </span>
+                  </span>
+                </p>
+              </li>
+
+              <li>
+                <p>
+                  Modélisation 3D:
+                  <span class="rond">
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr4"> </span>
+                  </span>
+                </p>
+              </li>
+
+              <li>
+                <p>
+                  Suite Office / Word:
+                  <span class="rond">
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr3"> </span>
+                    <span class="sr4"> </span>
+                  </span>
+                </p>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </article>
+    </div>
+  </body>
+</html>
